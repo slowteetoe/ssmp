d75d6b47a41911e56c6b65a460e8e03d8226f9ec
Steven Lotito <slotito@zappos.com>
2013-01-07 22:09:31 -0800

    Saves encrypted message to db now, have to add emails

--- app/controllers/message_controller.rb
+++ app/controllers/message_controller.rb
@@ -1,25 +1,65 @@
 class MessageController < ApplicationController
+  include EncryptionHelper
 
   require 'word_salad'
 
   def index
     @message_submission = MessageSubmission.new
     @message_submission.secret = 2.words.join("")
+    flash[:error] = nil
   end
 
   def view
-    @message = Message.find_by_id( params[:id] ) unless params[:id].nil?
+    @message = Message.find_by_uuid( params[:uuid] ) unless params[:uuid].nil?
+    if @message.nil?
+      @message = Message.new
+      flash[:error] = "Uhoh, that message doesn't seem to exist.  Please double-check"
+    end
   end
 
-  def decrypt
-    render :json => "This is my message"
+  def decrypt_message
+    logger.info "Attempting to decrypt uuid[#{params[:uuid]}]"
+    # this needs to try decrypting a given message using the provided secret
+    @message = Message.find_by_uuid( params[:uuid] )
+
+    if @message.nil?
+      render :json => {:msg => "Unknown message" }
+      return
+    end
+
+    begin
+      logger.info "Attempting to decrypt #{@message.contents} using #{params[:secret]}"
+      @decrypted = decrypt( params[:secret], @message.contents )
+      @message.decryption_attempts = 0
+    rescue
+      logger.error "Decryption failed #{$!}"
+      # it must increment attempt count for all failures as well
+      @message.decryption_attempts += 1
+      @decrypted = "Invalid Passphrase"
+    end
+    @message.save
+    render :json => { :msg => @decrypted }
   end
 
   def create
     @message_submission = MessageSubmission.new(params[:message_submission])
     @message_submission ||= MessageSubmission.new
     if @message_submission.valid?
-      render :json => @message_submission
+      m = Message.new
+      # encrypt the message with the passphrase
+      encrypted_val = encrypt( @message_submission.secret, @message_submission.content )
+      logger.info "The encrypted message is: #{encrypted_val}"
+      # save the message
+      m.contents = encrypted_val
+
+      m.save!
+
+      # send email
+
+      # send sms (if phone # provided)
+
+      # redirect to a success page
+      render :json => @m
     else
       flash[:error] = "There were errors with your submission, please check the form"
       render :action => :index
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,11 @@
 class Message < ActiveRecord::Base
-  include EncryptionHelper
-
   attr_accessible :contents, :creation_date, :decryption_attempts, :expires_at, :id, :sender, :uuid
+
+  before_create :generate_hash
+
+  def generate_hash
+    self.uuid = Digest::SHA1.hexdigest("#{Time.now},#{contents}")
+    logger.info "hash is #{@uuid}"
+  end
 
 end
